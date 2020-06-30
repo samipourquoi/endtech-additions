3beca0d4e7ee3e57069f5bce14e893e086e092be
samipourquoi <sami.guiderdoni@gmail.com>
2020-06-30 10:46:29 +0200

    Configure project files

---
+++ src/main/java/io/github/samipourquoi/etadditions/Main.java
@@ -0,0 +1,11 @@
+package io.github.samipourquoi.etadditions;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
