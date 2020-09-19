bdb5258dd97d6226028ad93a41a7fe37aad3a4b1
samipourquoi <sami.guiderdoni@gmail.com>
2020-09-19 13:28:42 +0200

    Remove ice from dig scoreboards

--- src/main/java/io/github/samipourquoi/endtech/mixin/MixinBlock.java
+++ src/main/java/io/github/samipourquoi/endtech/mixin/MixinBlock.java
@@ -1,8 +1,10 @@
 package io.github.samipourquoi.endtech.mixin;
 
 import io.github.samipourquoi.endtech.helpers.DigCriteriasAccessor;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -19,38 +21,40 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinBlock {
     @Inject(method = "afterBreak", at = @At("HEAD"))
     private void incrementDigScoreboards(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity, ItemStack stack, CallbackInfo info) {
-        player.incrementStat(
-            DigCriteriasAccessor
-                .getDig()
-                .getOrCreateStat(new Identifier("endtech", "all"))
-        );
-
-        String toolName = Registry.ITEM
-                .getId(player.getMainHandStack().getItem())
-                .getPath();
-
-        if (toolName.contains("pickaxe")) {
+        if (!((Block)(Object)this).equals(Blocks.ICE)) {
             player.incrementStat(
-                DigCriteriasAccessor
-                    .getDig()
-                    .getOrCreateStat(new Identifier("endtech", "picks"))
+                    DigCriteriasAccessor
+                            .getDig()
+                            .getOrCreateStat(new Identifier("endtech", "all"))
             );
-        }
 
-        if (toolName.contains("shovel")) {
-            player.incrementStat(
-                DigCriteriasAccessor
-                    .getDig()
-                    .getOrCreateStat(new Identifier("endtech", "shovels"))
-            );
-        }
+            String toolName = Registry.ITEM
+                    .getId(player.getMainHandStack().getItem())
+                    .getPath();
 
-        if (toolName.contains("axe")) {
-            player.incrementStat(
-                DigCriteriasAccessor
-                    .getDig()
-                    .getOrCreateStat(new Identifier("endtech", "axes"))
-            );
+            if (toolName.contains("pickaxe")) {
+                player.incrementStat(
+                        DigCriteriasAccessor
+                                .getDig()
+                                .getOrCreateStat(new Identifier("endtech", "picks"))
+                );
+            }
+
+            if (toolName.contains("shovel")) {
+                player.incrementStat(
+                        DigCriteriasAccessor
+                                .getDig()
+                                .getOrCreateStat(new Identifier("endtech", "shovels"))
+                );
+            }
+
+            if (toolName.contains("axe")) {
+                player.incrementStat(
+                        DigCriteriasAccessor
+                                .getDig()
+                                .getOrCreateStat(new Identifier("endtech", "axes"))
+                );
+            }
         }
     }
 }
