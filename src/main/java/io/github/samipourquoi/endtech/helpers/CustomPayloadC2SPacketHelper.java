a75fbe7b447137add93cff794ad04fdda29613a5
samipourquoi <sami.guiderdoni@gmail.com>
2020-09-26 22:56:59 +0200

    Remove lots of useless class

--- src/main/java/io/github/samipourquoi/endtech/helpers/CustomPayloadC2SPacketHelper.java
+++
@@ -1,9 +0,0 @@
-package io.github.samipourquoi.endtech.helpers;
-
-import net.minecraft.network.PacketByteBuf;
-import net.minecraft.util.Identifier;
-
-public interface CustomPayloadC2SPacketHelper {
-    Identifier getChannel();
-    PacketByteBuf getData();
-}
