99e6aab98aa0ffc6a144146ccb677bdfe5a11743
Syntro42 <syntro42@gmail.com>
2021-08-18 15:41:24 -0500

    Fix gradle not building

--- src/main/java/io/github/samipourquoi/endtech/mixin/MixinScoreboardTotal.java
+++ src/main/java/io/github/samipourquoi/endtech/mixin/MixinScoreboardTotal.java
@@ -10,6 +10,7 @@ import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,7 +21,7 @@ import java.util.List;
 
 @Mixin(ServerScoreboard.class)
 public class MixinScoreboardTotal extends Scoreboard {
-    @Final private MinecraftServer server;
+    @Shadow @Final private MinecraftServer server;
 
     @Inject(method = "updateScore", at = @At("TAIL"))
     public void updateScoreboardTotal(ScoreboardPlayerScore score, CallbackInfo ci) {
@@ -38,7 +39,7 @@ public class MixinScoreboardTotal extends Scoreboard {
 
     public ScoreboardPlayerUpdateS2CPacket scoreboardTotalPacket(ScoreboardObjective objective) {
         int totalScore = 0;
-        Iterator<ScoreboardPlayerScore> scores = this.getAllPlayerScores(objective).iterator();;
+        Iterator<ScoreboardPlayerScore> scores = this.getAllPlayerScores(objective).iterator();
 
         while(scores.hasNext()) {
             totalScore += scores.next().getScore();
